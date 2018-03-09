85446151074c74f355ddd68dade76358a43bbb28
Harsharadhya B K <34999060+hbkcad@users.noreply.github.com>
2018-03-09 12:22:38 +0530

    Update HelloWorldServlet.java

--- src/main/java/com/boraji/tutorial/servlet/HelloWorldServlet.java
+++ src/main/java/com/boraji/tutorial/servlet/HelloWorldServlet.java
@@ -14,7 +14,7 @@ import javax.servlet.http.HttpServletResponse;
 public class HelloWorldServlet extends HttpServlet {
 
    private static final long serialVersionUID = 1L;
-
+   int i=0;
    @Override
    protected void doGet(HttpServletRequest req, HttpServletResponse resp) 
          throws ServletException, IOException {
